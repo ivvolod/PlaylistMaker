45e0cb1c3257a1e0f7d4b798781d4be1431408ce
Vladimir <ivannikov.volod@gmail.com>
2023-12-03 22:25:11 +0300

    sprint8_final

---
+++ app/src/main/java/net/ru/ivvolod/playlistmaker/SearchActivity.kt
@@ -0,0 +1,12 @@
+package net.ru.ivvolod.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+    }
+}
