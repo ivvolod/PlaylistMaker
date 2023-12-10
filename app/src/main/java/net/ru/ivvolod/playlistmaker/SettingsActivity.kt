ae3f5dbe8888f336e1b64af70f7d8abc69f63659
Vladimir <ivannikov.volod@gmail.com>
2023-12-10 19:00:01 +0300

    feat(SettingActivity):added three intents: Share app, Write to support, User agreement

--- app/src/main/java/net/ru/ivvolod/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/net/ru/ivvolod/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package net.ru.ivvolod.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -19,5 +21,30 @@ class SettingsActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+        //Реализация кнопки «Поделиться приложением»
+        val btnShare = findViewById<TextView>(R.id.txv_share)
+        btnShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            val shareMessage = "Проверьте курс по Андроид-разработке в Практикуме: https://practicum.yandex.ru/profile/android-developer/"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+            startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"))
+        }
+        //Реализация кнопки «Написать в поддержку»
+        val btnSupport = findViewById<TextView>(R.id.txv_support)
+        btnSupport.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:ivannikov.vladimir.v@yandex.ru")
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            supportIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            startActivity(Intent.createChooser(supportIntent, "Написать в поддержку"))
+        }
+        //Реализация кнопки «Пользовательское соглашение»
+        val btnUserAgreement = findViewById<TextView>(R.id.txv_user_agreement)
+        btnUserAgreement.setOnClickListener {
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(userAgreementIntent)
+        }
+
     }
 }
