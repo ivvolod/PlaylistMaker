f6c53be9be4d4283a5f1d14eaca0ffca0171c070
Vladimir <ivannikov.volod@gmail.com>
2023-12-16 11:07:32 +0300

    feat(activity_search): added UI and search logic

--- app/src/main/java/net/ru/ivvolod/playlistmaker/SearchActivity.kt
+++ app/src/main/java/net/ru/ivvolod/playlistmaker/SearchActivity.kt
@@ -2,11 +2,56 @@ package net.ru.ivvolod.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        // Добавление тулбара
+        val toolbar: Toolbar = findViewById(R.id.SearchToolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
+        val searchEditText = findViewById<EditText>(R.id.editText)
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
+                // Перед изменением текста
+            }
+
+            override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                // В процессе изменения текста
+                if (charSequence.isNullOrEmpty()) {
+                    clearButton.visibility = View.INVISIBLE
+                } else {
+                    clearButton.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                // После изменения текста
+            }
+        })
+
+        clearButton.setOnClickListener {
+            searchEditText.text?.clear()
+            clearButton.visibility = View.INVISIBLE
+        }
+
 
     }
 }
