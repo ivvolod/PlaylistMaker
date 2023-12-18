4d6451217b824238e60bbf35eb587b5811a89dc2
Vladimir <ivannikov.volod@gmail.com>
2023-12-18 06:48:16 +0300

    feat(SearchActivity): saving state from EditText

--- app/src/main/java/net/ru/ivvolod/playlistmaker/SearchActivity.kt
+++ app/src/main/java/net/ru/ivvolod/playlistmaker/SearchActivity.kt
@@ -12,6 +12,12 @@ import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 
 class SearchActivity : AppCompatActivity() {
+    //Создание константных переменных для сохранения состояния
+    companion object {
+        private const val SEARCH_TEXT_KEY = "SEARCH_TEXT_KEY"
+        private const val DEFAULT_SEARCH_TEXT = ""
+    }
+    private var searchText: String = DEFAULT_SEARCH_TEXT
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -28,12 +34,16 @@ class SearchActivity : AppCompatActivity() {
         val searchEditText = findViewById<EditText>(R.id.editText)
         val clearButton = findViewById<ImageView>(R.id.clearButton)
 
+        // Добавление TextWatcher для отслеживания изменений в EditText
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
                 // Перед изменением текста
             }
 
             override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                // Сохранение значения в переменной
+                searchText = charSequence.toString()
+
                 // В процессе изменения текста
                 if (charSequence.isNullOrEmpty()) {
                     clearButton.visibility = View.INVISIBLE
@@ -43,7 +53,7 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(editable: Editable?) {
-                // После изменения текста
+                // Ничего не делаем после изменения текста
             }
         })
 
@@ -51,7 +61,25 @@ class SearchActivity : AppCompatActivity() {
             searchEditText.text?.clear()
             clearButton.visibility = View.INVISIBLE
         }
+    }
+    //Сохранние состояния
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Сохранение значения из EditText в Bundle
+        outState.putString(SEARCH_TEXT_KEY, searchText)
+    }
+    //Восстановление состояния
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Восстановление значения из Bundle
+        val restoredText = savedInstanceState.getString(SEARCH_TEXT_KEY)
 
-
+        // Проверка на null
+        if (restoredText != null) {
+            searchText = restoredText
+            val searchEditText = findViewById<EditText>(R.id.editText)
+            // Установка восстановленного текста в EditText
+            searchEditText.setText(searchText)
+        }
     }
 }
